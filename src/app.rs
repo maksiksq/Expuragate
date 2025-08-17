7ff203a36975c60fde5575922ce3ff12be908afe
Maksiks <maksiksking@gmail.com>
2025-08-17 21:01:39 +0300

    chore: removed some more boilerplate

--- src/app.rs
+++ src/app.rs
@@ -300,17 +300,6 @@ impl eframe::App for Expurgate {
             // The top panel is often a good place for a menu bar:
 
             egui::MenuBar::new().ui(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
                 egui::widgets::global_theme_preference_buttons(ui);
             });
         });
